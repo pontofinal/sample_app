1d14c4f32a13e6128f2062afae4bba51c8b02974
Ricardo <bonfocchi@gmail.com>
2012-10-16 17:29:01 +0100

    Add user microposts

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -16,6 +16,23 @@ describe "Static pages" do
 
     it_should_behave_like "all static pages"
     it { should_not have_selector 'title', text: '| Home' }
+
+    describe "for signed-in users" do
+      let(:user) { FactoryGirl.create(:user) }
+      before do
+        FactoryGirl.create(:micropost, user: user, content: "Lorem ipsum")
+        FactoryGirl.create(:micropost, user: user, content: "Dolor sit amet")
+        sign_in user
+        visit root_path
+      end
+
+      it "should render the user's feed" do
+        user.feed.each do |item|
+          page.should have_selector("li##{item.id}", text: item.content)
+        end
+      end
+    end
+
   end
 
   describe "Help page" do
@@ -38,19 +55,19 @@ describe "Static pages" do
     it { should have_selector('h1',    text: 'Contact') }
     it { should have_selector('title', text: full_title('Contact')) }
   end
-  
+
   it "should have the right links on the layout" do
-     visit root_path
-     click_link "About"
-     page.should have_selector 'title', text: full_title('About Us')
-     click_link "Help"
-     page.should have_selector 'title', text: full_title('Help')
-     click_link "Contact"
-     page.should have_selector 'title', text: full_title('Contact')
-     click_link "Home"
-     click_link "Sign up now!"
-     page.should have_selector 'title', text: full_title('')
-     click_link "sample app"
-     page.should_not have_selector 'title', text: full_title('| Home')
-   end
+    visit root_path
+    click_link "About"
+    page.should have_selector 'title', text: full_title('About Us')
+    click_link "Help"
+    page.should have_selector 'title', text: full_title('Help')
+    click_link "Contact"
+    page.should have_selector 'title', text: full_title('Contact')
+    click_link "Home"
+    click_link "Sign up now!"
+    page.should have_selector 'title', text: full_title('')
+    click_link "sample app"
+    page.should_not have_selector 'title', text: full_title('| Home')
+  end
 end
