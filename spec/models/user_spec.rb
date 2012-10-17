85507508c65e18a4326125781b7163086175c27e
Ricardo <bonfocchi@gmail.com>
2012-10-17 16:02:05 +0100

    Add user following

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -28,6 +28,13 @@ describe User do
   it { should respond_to(:admin) }
   it { should respond_to(:microposts) }
   it { should respond_to(:feed) }
+  it { should respond_to(:relationships) }
+  it { should respond_to(:followed_users) }
+  it { should respond_to(:reverse_relationships) }
+  it { should respond_to(:followers) }  
+  it { should respond_to(:following?) }
+  it { should respond_to(:follow!) }
+  it { should respond_to(:unfollow!) }
   it { should respond_to(:authenticate) }
 
   it { should be_valid }
@@ -138,8 +145,8 @@ describe User do
       before { @user.save }
       its(:remember_token) { should_not be_blank }
     end
-    
-    
+
+
     describe "micropost associations" do
 
       before { @user.save }
@@ -162,17 +169,41 @@ describe User do
           Micropost.find_by_id(micropost.id).should be_nil
         end
       end
-      
-      describe "status" do
-           let(:unfollowed_post) do
-             FactoryGirl.create(:micropost, user: FactoryGirl.create(:user))
-           end
 
-           its(:feed) { should include(newer_micropost) }
-           its(:feed) { should include(older_micropost) }
-           its(:feed) { should_not include(unfollowed_post) }
+      describe "status" do
+        let(:unfollowed_post) do
+          FactoryGirl.create(:micropost, user: FactoryGirl.create(:user))
+        end
+
+        its(:feed) { should include(newer_micropost) }
+        its(:feed) { should include(older_micropost) }
+        its(:feed) { should_not include(unfollowed_post) }
+      end
+
+    end
+
+    describe "following" do
+      let(:other_user) { FactoryGirl.create(:user) }    
+      before do
+        @user.save
+        @user.follow!(other_user)
+      end
+
+      it { should be_following(other_user) }
+      its(:followed_users) { should include(other_user) }
+
+      describe "and unfollowing" do
+        before { @user.unfollow!(other_user) }
+
+        it { should_not be_following(other_user) }
+        its(:followed_users) { should_not include(other_user) }
+      end
+      
+      describe "followed user" do
+        subject { other_user }
+        its(:followers) { should include(@user) }
       end
       
     end
-    
+
   end
